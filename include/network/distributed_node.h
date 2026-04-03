4220cb9e1253d79bfe6a719b1292b674c9abe0dd
idcu <idcu@qq.com>
2026-04-04 07:36:29 +0800

    实现节点间消息传递

--- include/network/distributed_node.h
+++ include/network/distributed_node.h
@@ -2,6 +2,7 @@
 #define IDCU_NETWORK_DISTRIBUTED_NODE_H
 
 #include "common/error_code.h"
+#include "network/network_layer.h"
 #include <stdint.h>
 #include <stddef.h>
 
@@ -10,6 +11,9 @@
 #define IDCU_MAX_MESSAGE_PAYLOAD_SIZE 1024
 #define IDCU_NODE_NAME_MAX 64
 #define IDCU_NODE_ADDR_MAX 64
+#define IDCU_MAX_CONNECTIONS 16
+#define IDCU_MESSAGE_MAGIC 0x49444355  // "IDCU" in hex
+#define IDCU_MESSAGE_VERSION 1
 
 typedef enum {
     IDCU_NODE_STATUS_ONLINE = 0,
@@ -26,14 +30,18 @@ typedef struct {
     idcu_NodeStatus status;
     uint64_t load;
     uint64_t last_heartbeat;
+    idcu_NetworkSocket socket;
+    int connected;
 } idcu_NodeInfo;
 
 typedef struct {
+    uint32_t magic;
+    uint32_t version;
     uint64_t from_node;
     uint64_t to_node;
     uint32_t type;
+    uint32_t payload_size;
     uint8_t payload[IDCU_MAX_MESSAGE_PAYLOAD_SIZE];
-    size_t payload_size;
 } idcu_NodeMessage;
 
 typedef struct {
@@ -44,6 +52,8 @@ typedef struct {
     int msg_count;
     int msg_head;
     int msg_tail;
+    idcu_NetworkServer server;
+    int server_started;
 } idcu_DistributedNode;
 
 int idcu_distributed_node_init(idcu_DistributedNode* node, uint64_t node_id, const char* name, const char* address, uint16_t port);
@@ -56,5 +66,10 @@ int idcu_distributed_node_update_heartbeat(idcu_DistributedNode* node, uint64_t 
 int idcu_distributed_node_send_message(idcu_DistributedNode* node, uint64_t to_node, uint32_t type, const void* payload, size_t payload_size);
 int idcu_distributed_node_recv_message(idcu_DistributedNode* node, idcu_NodeMessage* msg);
 uint64_t idcu_distributed_node_select_node_by_load(idcu_DistributedNode* node);
+int idcu_distributed_node_start_server(idcu_DistributedNode* node);
+int idcu_distributed_node_stop_server(idcu_DistributedNode* node);
+int idcu_distributed_node_connect_to_node(idcu_DistributedNode* node, uint64_t node_id);
+int idcu_distributed_node_disconnect_from_node(idcu_DistributedNode* node, uint64_t node_id);
+int idcu_distributed_node_process(idcu_DistributedNode* node);
 
 #endif // IDCU_NETWORK_DISTRIBUTED_NODE_H
